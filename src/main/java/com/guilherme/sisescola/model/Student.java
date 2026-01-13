0ae0ef436b0db94433019317aa3b09538832c01d
marcosfernandesg <contatomarcosfg@gmail.com>
2026-01-13 12:51:59 -0300

    feat: create Student entity with relationship to SchoolClass

---
+++ src/main/java/com/guilherme/sisescola/model/Student.java
@@ -0,0 +1,16 @@
+package com.guilherme.sisescola.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    private SchoolClass schoolClass;
+}
